3614499cb47f2ae7dbf293cc2e80d45bf44a4ddf
vlncceeks <vlncceeks@gmail.com>
2025-02-15 22:46:27 +0300

    lab 1

---
+++ lab1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Мамонова Ксения лабораторная 1</title>
+</head>
+<body>
+    <header>
+        <img src="logo.svg" style="filter: brightness(0)" width=200px alt="logo_polytech">
+        <p>2.1.Домашняя работа: Hello, World!</p>
+    </header>
+    <main>
+        <p><?php echo "Hello, world!"?></p>
+    </main>
+    <footer>
+        <p> <?php
+            $cssFile = "style.css";
+            $css = file_get_contents($cssFile);
+            if ($css === FALSE) {
+                die("Непрапвильно указан путь к CSS-файлу");
+            }
+            $propertyCount = strlen($css);
+            echo "Количество символов в файле CSS: $propertyCount";
+            ?> 
+        </p>
+    </footer>
+</body>
